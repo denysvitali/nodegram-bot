fcefb23798909cee39f8fddc18b73eb9e0b52d5a
Denys Vitali <denys.vitali@coelis.ch>
2015-06-25 02:45:21 +0200

    Added default settings.json

---
+++ settings.js
@@ -0,0 +1,13 @@
+/*
+	This is your settings file
+*/
+
+var settings = {
+	debug: true,
+	token: "YOUR TOKEN",
+	api: "https://api.telegram.org",
+	hostname: "denvit.work"
+	port: 30303
+
+};
+module.exports = settings;
